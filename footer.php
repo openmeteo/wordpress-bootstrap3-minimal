266a650caabe7132d1e98bb1640d9cfaaf956760
Antonis Christofides <antonis@antonischristofides.com>
2017-12-04 22:37:24 +0200

    Add footer

--- footer.php
+++ footer.php
@@ -2,4 +2,28 @@
         </div>
     </div>
 </main>
+<footer style="position: absolute; bottom: 0; left:0; width: 100%; margin-bottom: 0 !important;" class="well well-sm text-center">
+  <ul class="list-inline small" style="padding-top: 1em">
+    <li>
+      <img alt="Ευρωπαϊκή Ένωση" width="100" height="70"
+      src="<?php echo get_theme_file_uri('/assets/images/eu.jpg');?>">
+      <p>Ευρωπαϊκή Ένωση<br>Ευρωπαϊκό Ταμείο Περιφερειακής Ανάπτυξης</p>
+    </li>
+    <li></li>
+    <li>
+      <img alt="ΕΣΠΑ 2014-2020" width="100" height="70"
+      src="<?php echo get_theme_file_uri('/assets/images/espa.jpg')?>">
+      <p>&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+    </li>
+  </ul>
+
+  <p>Με τη συγχρηματοδότηση της Ελλάδας και της Ευρωπαϊκής Ένωσης</p>
+
+  <ul class="list-inline small" style="padding-top: 1em">
+    <li><a href="http://main.hydroscope.gr/" class="text-muted">Δεδομένα</a></li>
+    <li><a href="/software" class="text-muted">Λογισμικά</a></li>
+    <li><a href="/documents" class="text-muted">Έγγραφα</a></li>
+    <li><a href="/contact/" class="text-muted">Επικοινωνία</a></li>
+  </ul>
+</footer>
 <?php wp_footer();?>
